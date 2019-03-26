9d14a461d45508e4bdabe11d3a0aa2304a00a7e6
musebe <emusebe9@gmail.com>
2019-03-26 12:12:23 +0000

    Updates

---
+++ precache-manifest.fda0ae85276704306978d24f7d280963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaeac616ecbcb9149f74",
+    "url": "/react_tic_tac/static/js/runtime~main.336fd85f.js"
+  },
+  {
+    "revision": "2d7e277d1d1f2e096ee5",
+    "url": "/react_tic_tac/static/js/main.2131d3e2.chunk.js"
+  },
+  {
+    "revision": "b7369efbbeb2437189ed",
+    "url": "/react_tic_tac/static/js/2.543df2ed.chunk.js"
+  },
+  {
+    "revision": "2d7e277d1d1f2e096ee5",
+    "url": "/react_tic_tac/static/css/main.ba04ca16.chunk.css"
+  },
+  {
+    "revision": "32932035efb34d41c0e2ab744f665c42",
+    "url": "/react_tic_tac/index.html"
+  }
+];
